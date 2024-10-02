00fda6635192898d07a0f360a6bc6ef5672a1e5e
akash1675 <captainakash1675@gmail.com>
2024-10-03 01:03:31 +0530

    commit

--- projects/calculator-app/src/App.jsx
+++ projects/calculator-app/src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
 import styles from "./App.module.css"
 import ButtonContainer from "./components/ButtonContainer";
 import Display from "./components/Display";
 
+
 function App() {
+  const [calval, setCalVal] = useState("sex");
+  const onButtonClick = (buttonText) => {
+    if(buttonText === 'C'){
+      setCalVal("");
+    }
+    else if(buttonText === '='){
+      const result = eval(calval);
+      setCalVal(result);
+    }
+    else{
+      const newDisplayVal = calval + buttonText;
+      setCalVal(newDisplayVal);
+    }
+  }
   
   return (
     <>
     <div className={styles.calculator}>
-     <Display></Display>
-      <ButtonContainer></ButtonContainer>
+     <Display displayValue = {calval}></Display>
+      <ButtonContainer onButtonClick = {onButtonClick}></ButtonContainer>
     </div>
     </>
   );
